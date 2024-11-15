a99e0f3c8399f484cccddc63812f5bc6b61d7bbb
Laurette <133497840+OraTeta11@users.noreply.github.com>
2024-11-15 14:49:10 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,9 +1,3 @@
-// document.getElementById("count").innerText = 5
-
-// let count = 5
-// count = count + 1
-// console.log(count)
-
 // intialize the count as 0
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked (log it out)
